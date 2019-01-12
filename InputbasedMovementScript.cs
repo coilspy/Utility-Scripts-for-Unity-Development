1a586f9e984ee8b31ff1ea1667b26783bbab99c4
coilspy <max.gw@live.se>
2019-01-12 23:18:08 +0100

    Added Inputbased movement via vector math and rigidbody force calculations.

---
+++ InputbasedMovementScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputbasedMovementScript : MonoBehaviour {
+
+    public float Speed;
+        
+	void FixedUpdate () {
+        this.transform.position += new Vector3(Input.GetAxis("Horizontal") * Speed, 0, Input.GetAxis("Vertical") * Speed) * Time.deltaTime;
+	}
+}
---
+++ InputbasedRigidbodyForceMovementScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class InputbasedRigidbodyForceMovementScript : MonoBehaviour {
+    public float Speed;
+    private Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+        rigidbody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {   
+        float horizontalAxisValue = Input.GetAxis("Horizontal");
+        float verticalAxisValue = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = new Vector3(horizontalAxisValue, 0, verticalAxisValue);
+        rigidbody.AddForce(movementVector * Speed);
+	}
+}
